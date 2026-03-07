603657f4f78167e01ca1ca49980502aa182d7899
Roselle Malig-on <rosellemarie.malig-on@cit.edu>
2026-03-07 19:28:32 +0800

    push

--- src/models/Patient.java
+++ src/models/Patient.java
@@ -1,5 +1,24 @@
 package models;
 
-public class Patient {
+public class Patient extends Person{
+    private String patientId;
+
+    public Patient(String name, int age, String gender, String ContactNumber, String patientId){
+        super(name, age, gender, ContactNumber);
+        this.patientId = patientId;
+    }
+
+    @Override
+    public void displayInfo(){
+        System.out.println("Patient ID: " + patientId);
+        System.out.println("Name: " + getName());
+        System.out.println("Age: " + getAge());
+        System.out.println("Gender: " + getGender());
+        System.out.println("Contact Number: " + getContactNumber());
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
     
 }

7a1e3ffa6333ca80702c68212b2c00845bdccd1f
Roselle Malig-on <rosellemarie.malig-on@cit.edu>
2026-03-07 14:40:50 +0800

    Push

--- src/management/PatientManager.java
+++ src/management/PatientManager.java
@@ -1,5 +1,5 @@
 package management;
 
 public class PatientManager {
-    
+   
 }
--- src/models/Person.java
+++ src/models/Person.java
@@ -1,5 +1,46 @@
 package models;
 
 public class Person {
-    
+    private String Name;
+    private int Age;
+    private String Gender;
+    private String ContactNumber;
+
+    public Person(String Name, int Age, String Gender, String ContactNumber){
+        this.Name = Name;
+        this.Age = Age;
+        this.Gender = Gender;
+        this.ContactNumber = ContactNumber;
+    }
+
+    public String getName(){
+        return Name;
+    }
+    public void setName(String Name){
+        this.Name = Name;
+    }
+
+    public int getAge(){
+        return Age;
+    }
+    public void setAge(int Age){
+        this.Age = Age;
+    }
+
+    public String getGender(){
+        return Gender;
+    }
+    public void setGender(String Gender){
+        this.Gender = Gender;
+    }
+
+    public String getContactNumber(){
+        return ContactNumber;
+    }
+    public void setContactNumber(String ContactNumber){
+        this.ContactNumber = ContactNumber;
+    }
+
+
+
 }

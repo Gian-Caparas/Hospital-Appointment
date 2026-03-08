0cf8ebdb395b2dd0ae2970eaccf576db8ef3dfb1
Melvin Luije Rafaela <melvinrafaela16@gmail.com>
2026-03-08 14:27:36 +0800

    Push

--- src/management/DepartmentManager.java
+++ src/management/DepartmentManager.java
@@ -1,5 +1,22 @@
 package management;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Department;
+
 public class DepartmentManager {
     
+    // Creates List of all Departments and calls it departments
+    private List<Department> departments = new ArrayList<>();
+
+    // creates and adds a new department
+    public void createDepartment(Department department) {//stores the variable parameter(calls it "department") in the list of departments.
+        departments.add(department); //adds the variable("department") to the list of departments
+    }
+
+    // returns the list of departments
+    public List<Department> getDepartments() {
+        return departments;
+    }
 }
--- src/models/Appointment.java
+++ src/models/Appointment.java
@@ -1,5 +1,55 @@
 package models;
 
 public class Appointment {
+    private Patient patient;
+    private Doctor doctor;
+    private String date;
+
+    public Appointment(Patient patient, Doctor doctor, String date){
+        this.patient = patient;
+        this.doctor = doctor;
+        this.date = date;
+    }
+
+    public void displayAppointment(){
+        System.out.println("===== Appointment Details: ====="); //kailangan pa og design?
+        System.out.println("Patient: " + patient.getName()); //Name ra i-display?
+        //patient.displayInfo(); // OR kania gamiton? like para display ang whole info sa patient.
+        System.out.println("Doctor-in-charge: " + doctor.getName()); // If mag getName sa doctor, dapat mag add og getName method sa Doctor class
+        System.out.println("Date: " + date);
+    }
+
+
+
+    /* In case mag need  // Delete if not needed
     
+    Getters and setters 
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    */
+
+
 }
